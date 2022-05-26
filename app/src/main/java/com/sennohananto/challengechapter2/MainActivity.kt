9fb642c3f4d0523e23e511a1660b044041118bd8
Senno Hananto <sennohananto@gmail.com>
2022-05-26 15:29:49 +0700

    Initial commit

---
+++ app/src/main/java/com/sennohananto/challengechapter2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sennohananto.challengechapter2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
